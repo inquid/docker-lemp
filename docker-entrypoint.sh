db3a06316b43bad080324a498c2d260182e55116
Luis Gonzalez <luisarmando1234@gmail.com>
2021-03-28 00:14:55 -0600

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -42,7 +42,7 @@ if [ ! -f "/run/mysqld/.init" ]; then
   touch /run/mysqld/.init
 fi
 
+mongod --dbpath /data/db run
 mongo < /data/admin.js
-mongod --auth --port 27017 --dbpath /data/db run
 
 exec "$@"
